0c8456ac1e94d3a7ae8f83047ca62312efcd262f
April Long <april11dance@gmail.com>
2019-10-28 22:41:01 -0600

    routes and js files underway

--- models/Article.js
+++ models/Article.js
@@ -1,1 +1,44 @@
 //use mongoose table// 
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new LibrarySchema object
+// This is similar to a Sequelize model
+var hiphopSchema = new Schema
+
+
+var headlineSchema = new schema ({
+    title: {
+        type: "String",
+        unique: true, 
+        required: true,
+        trim: true
+    },
+
+    url: {
+        type: "String",
+        required: "not a valid url",
+        
+    },
+
+    summary: {
+        type: "String",
+        required: true,
+        trim: true
+
+    }, 
+
+    note: {
+
+    },
+
+    saved: {
+        type: Boolean,
+        default: false,
+        
+    }
+});
+
+var headline = mongoose.model("headline", headlineSchema)
